8ae54c589f92f8adab1d3a00d6a7e526470b1320
Ashutosh <ash7594@gmail.com>
2015-01-23 12:44:51 +0400

    minor change

--- user/config.php
+++ user/config.php
@@ -29,7 +29,7 @@ define( 'YOURLS_DB_PREFIX', 'shortURL_' );
 
 /** YOURLS installation URL -- all lowercase and with no trailing slash.
  ** If you define it to "http://site.com", don't use "http://www.site.com" in your browser (and vice-versa) */
-define( 'YOURLS_SITE', 'localhost' );
+define( 'YOURLS_SITE', 'http://localhost/shortURL' );
 
 /** Timezone GMT offset */
 define( 'YOURLS_HOURS_OFFSET', 0 ); 
